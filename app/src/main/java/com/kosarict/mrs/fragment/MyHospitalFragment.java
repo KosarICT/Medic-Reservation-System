5c725753beaa5187331310d071da2f08ce06b7b4
Siavash <s.sajjad1370@gmail.com>
2016-08-10 15:41:27 +0430

    version 1.0.9

---
+++ app/src/main/java/com/kosarict/mrs/fragment/MyHospitalFragment.java
@@ -0,0 +1,45 @@
+package com.kosarict.mrs.fragment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kosarict.mrs.R;
+
+
+public class MyHospitalFragment extends Fragment {
+    private View layoutView;
+    private FragmentActivity context;
+
+
+    public MyHospitalFragment() {
+    }
+
+    public static MyHospitalFragment newInstance() {
+        return new MyHospitalFragment();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        context = (FragmentActivity) activity;
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        layoutView = inflater.inflate(R.layout.fragment_my_hospital, container, false);
+        return layoutView;
+    }
+
+}
